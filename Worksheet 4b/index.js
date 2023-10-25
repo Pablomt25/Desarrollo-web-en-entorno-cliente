22ea03d131d7a5881d37a383343618c54212215c
Pablo Moreno <pablomorenotiradomt25@gmail.com>
2023-10-25 09:26:30 +0200

    commit
    commit

--- Worksheet 4b/index.js
+++ Worksheet 4b/index.js
@@ -1,21 +1,44 @@
-window.onload = ()=>{
-    console.log("Pagina cargada")
-
+window.onload = () => {
+    console.log("Página cargada");
     asociarEventos();
+    mostrarElementos(6);
 }
 
-function asociarEventos(){
-
-    const id = document.getElementById("id");
-
+function asignarColoresAleatorios(ids) {
     function random(number) {
         return Math.floor(Math.random() * (number + 1));
-      }
-      
-      btn.addEventListener("click", () => {
-        const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-        document.body.style.backgroundColor = rndCol;
-      })
+    }
 
-      
+    ids.forEach((id) => {
+        const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+        id.style.backgroundColor = rndCol;
+    });
+}
+
+function asociarEventos() {
+    const easyLink = document.querySelector("a[href='#easy']");
+    const hardLink = document.querySelector("a[href='#hard']");
+    const ids = document.querySelectorAll(".id");
+    const newColorsLink = document.getElementById("newColorsLink");
+
+    newColorsLink.addEventListener("click", () => {
+        asignarColoresAleatorios(ids);
+    });
+
+    easyLink.addEventListener("click", () => {
+        mostrarElementos(3);
+    });
+
+    hardLink.addEventListener("click", () => {
+        mostrarElementos(6);
+    });
+}
+
+function mostrarElementos(cantidad) {
+    const ids = document.querySelectorAll(".id");
+
+
+    ids.forEach((id, index) => {
+        id.style.display = index < cantidad ? "block" : "none";
+    });
 }
